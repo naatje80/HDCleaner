786092c1eb2beec5af75a8bc6aa07092f443ee59
Sanders, Nathan <n.m.sanders@tue.nl>
2024-12-03 14:40:02 +0100

    Added test script to format NVME drives

---
+++ test_nvme.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+NVME_DEVICES=`nvme list -o json|grep 'DevicePath'|tr -d ' '|tr -d '"'|cut -d: -f 2`
+IFS=', ' read -r -a array <<< ${NVME_DEVICES}
+
+SERIALNUMBER=`nvme id-ctrl ${1}|tr -d ' '|grep 'sn:'|cut -d: -f2`
+
+# Do not use "Overwrite Erase" to save life endurance of NVME
+# Thefore this option is not verified
+SAN_BE_SUPPORTED=`nvme id-ctrl ${1} -H|grep 'Block Erase Sanitize Operation'|grep -c '0x1'`
+SAN_CE_SUPPORTED=`nvme id-ctrl ${1} -H|grep 'Crypto Erase Sanitize Operation'|grep -c '0x1'`
+FORM_CE_SUPPORTED=`nvme id-ctrl ${1} -H|grep 'Crypto Erase'|grep 'Secure Erase'|grep -c '0x1'`
+
+# Uncomment for debug
+#echo -e "DEBUG:--->\nSanitize Block Erase: ${SAN_BE_SUPPORTED}\nSantize Crypto Erase: ${SAN_CE_SUPPORTED}\nFormat Secure Erase: ${FORM_CE_SUPPORTED}"
+
+if [[ -n ${SAN_CE_SUPPORTED} ]]
+then
+	ERASE_COMMAND="nvme sanitze ${1} -a start-crypto-erase"
+	DURATION=`nvme sanitize-log ${1}|grep 'Estimated Time For Crypto Erase'|tr -d ' '|cut -d: -f2`
+elif [[ -n ${SA_BE_SUPPORTED} ]]
+then
+	ERASE_COMMAND="nvme sanitze ${1} -a start-block-erase"
+	DURATION=`nvme sanitize-log ${1}|grep 'Estimated Time For Block Erase'|tr -d ' '|cut -d: -f2`
+elif [[ -n ${FORM_CE_SUPPORTED} ]]
+then
+	if [[ `nvme id-ctrl ${1} -H|grep -E 'Format Applies to Single Namespace(s)|Crypto Erase Applies to Single Namespace(s)'|grep -c -v '0x1' == '2' ]]
+	then
+		ERASE_COMMAND="nvme format ${1} -s 2 -n 1"
+	fi
+else
+	ERASE_COMMAND="bkjdiscard --secure ${1}"
+if
+
+echo ${ERASE_COMMAND}
+
