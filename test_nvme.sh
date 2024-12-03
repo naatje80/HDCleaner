2702bfc2189b6595d7b4acdbb890f1ed9fbe50e5
Sanders, Nathan <n.m.sanders@tue.nl>
2024-12-03 15:09:41 +0100

    Some issues in the script

--- test_nvme.sh
+++ test_nvme.sh
@@ -14,23 +14,22 @@ FORM_CE_SUPPORTED=`nvme id-ctrl ${1} -H|grep 'Crypto Erase'|grep 'Secure Erase'|
 # Uncomment for debug
 #echo -e "DEBUG:--->\nSanitize Block Erase: ${SAN_BE_SUPPORTED}\nSantize Crypto Erase: ${SAN_CE_SUPPORTED}\nFormat Secure Erase: ${FORM_CE_SUPPORTED}"
 
-if [[ -n ${SAN_CE_SUPPORTED} ]]
+if [[ ${SAN_CE_SUPPORTED} -eq 1 ]]
 then
 	ERASE_COMMAND="nvme sanitze ${1} -a start-crypto-erase"
 	DURATION=`nvme sanitize-log ${1}|grep 'Estimated Time For Crypto Erase'|tr -d ' '|cut -d: -f2`
-elif [[ -n ${SA_BE_SUPPORTED} ]]
+elif [[ ${SA_BE_SUPPORTED} -eq 1 ]]
 then
 	ERASE_COMMAND="nvme sanitze ${1} -a start-block-erase"
 	DURATION=`nvme sanitize-log ${1}|grep 'Estimated Time For Block Erase'|tr -d ' '|cut -d: -f2`
-elif [[ -n ${FORM_CE_SUPPORTED} ]]
+elif [[ ${FORM_CE_SUPPORTED} -eq 1 ]]
 then
-	if [[ `nvme id-ctrl ${1} -H|grep -E 'Format Applies to Single Namespace(s)|Crypto Erase Applies to Single Namespace(s)'|grep -c -v '0x1' == '2' ]]
+	if [[ `nvme id-ctrl ${1} -H|grep -E 'Format Applies to Single Namespace\(s\)|Crypto Erase Applies to Single Namespace\(s\)'|grep -c -v '0x1'` -eq 1 ]]
 	then
 		ERASE_COMMAND="nvme format ${1} -s 2 -n 1"
 	fi
 else
-	ERASE_COMMAND="bkjdiscard --secure ${1}"
-if
+	ERASE_COMMAND="blkdiscard --secure ${1} -f|blkdiskcard --zeroout /${1} -f"
+fi
 
-echo ${ERASE_COMMAND}
-
+${ERASE_COMMAND}
